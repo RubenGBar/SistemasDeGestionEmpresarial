cdd9e7c2a8f132ddedbfe6850cf2bd14ce5b75bf
RubenGBar <ruben.garcia@iesnervion.es>
2024-10-15 14:17:27 +0200

    MVC comenzado

---
+++ ASPNETMVC/Ejemplo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using Ejemplo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejemplo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int Entero()
+        {
+            return 3;
+        }
+
+        public String Saludo()
+        {
+            return "Hola Mundo";
+
+        }
+            public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
